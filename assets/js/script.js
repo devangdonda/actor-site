d074a13c1feac5f5bd6664b24551c324c8a4b479
devangdonda <dkdonda@mun.ca>
2024-06-13 04:25:33 -0400

    commit-2

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function sendEmail() {
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        subject: document.getElementById("subject").value
+    }
+
+    emailjs.send("service_xto1di7", "template_ewqot38", params).then(alert("Email Sent!!"))
+}
+  
