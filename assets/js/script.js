8aa93f588d364be9338a5216a926a62ce0955600
Devang Donda <113135485+devangdonda@users.noreply.github.com>
2024-06-17 19:28:14 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,21 @@ function sendEmail() {
 
     emailjs.send("service_xto1di7", "template_ewqot38", params).then(alert("Email Sent!!"))
 }
+
+const audio = document.getElementById('backgroundMusic');
+    const playPauseButton = document.getElementById('playPauseButton');
+
+    // Set the initial volume to a lower level
+    audio.volume = 0.2;
+
+    // Add an event listener to the button to control playback
+    playPauseButton.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+            playPauseButton.textContent = 'Pause';
+        } else {
+            audio.pause();
+            playPauseButton.textContent = 'Play';
+        }
+    });
   
